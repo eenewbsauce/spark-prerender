b3d1ffc9b3234b7224f812c69051f17d0a82b725
eenewbsauce <ryan.m.tracey@gmail.com>
2018-12-12 16:08:19 -0500

    fix(spark): add dep

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-import sparkCorePrerender from "@sparkdesignsystem/spark-core/spark-core-prerender";
+import sparkCore from "@sparkdesignsystem/spark-core/es5/sparkCore"
+import sparkCorePrerender from "@sparkdesignsystem/spark-core/es5/sparkCorePrerender";
 
 sparkCorePrerender();
+sparkCore();
